eda688bb4de68cd2ff6e136d5aa497155d198261
Ilya Nesterov <eildos95@gmail.com>
2021-09-09 20:52:33 +0300

    register `Note` model, include notes urls

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Note, NoteAdmin)
