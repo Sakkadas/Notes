0708db965c9bf0bcb9d424ebd8fe4397e5e61558
Ilya Nesterov <eildos95@gmail.com>
2021-09-09 22:12:49 +0300

    create `NotesDetailView`, url paths, templates

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from django.urls.conf import include
 from .views import *
 
+app_name = 'notes'
 
 urlpatterns = [
     path("", NotesListView.as_view(), name='notes'),
+    path("view/<str:slug>/", NotesDetailView.as_view(), name='note'),
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import *
 
@@ -7,4 +8,14 @@ from .models import *
 class NotesListView(ListView):
     model = Note
     context_object_name = 'notes'
-    template_name = 'notes_list.html'
+    template_name = 'notes/notes_list.html'
+
+
+class NotesDetailView(DetailView):
+    model = Note
+    template_name = 'notes/notes_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(NotesDetailView, self).get_context_data(**kwargs)
+        note = get_object_or_404(Note, slug=self.kwargs['slug'])
+        context['note'] = note
