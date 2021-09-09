be67caa48e191c9b923e0f57d7b06cf67033752d
Ilya Nesterov <eildos95@gmail.com>
2021-09-09 23:01:58 +0300

    add form, fix typo

---
+++ notes/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import *
+
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = [
+            'title', 'text', 'source', 'summary',
+        ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 
 
@@ -7,37 +7,49 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import *
+from .forms import *
 
 
 class NotesListView(ListView):
     model = Note
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    success_url = reverse_lazy('notes:notes')
 
 
-class NotesDetailView(DetailView):
+class NoteDetailView(DetailView):
     model = Note
     template_name = 'notes/note_view.html'
+    context_object_name = 'note'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(NotesDetailView, self).get_context_data(**kwargs)
+        context = super(NoteDetailView, self).get_context_data(**kwargs)
         note = get_object_or_404(Note, slug=self.kwargs['slug'])
         context['note'] = note
 
-class NotesCreateView(CreateView):
+
+class NoteCreateView(CreateView):
     model = Note
-    success_url = reverse_lazy('notes')
-    
+    success_url = reverse_lazy('notes:notes')
+    form_class = NoteForm
+
     def form_valid(self, form):
         form.instance.author = self.request.user
-        return super(NotesCreateView, self).form_valid(form)
+        return super(NoteCreateView, self).form_valid(form)
+
 
 class NoteUpdateView(UpdateView):
     model = Note
-    template_name = 'notes/update_note.html'
+    success_url = reverse_lazy('notes:notes')
+    form_class = NoteForm
 
-class NotesDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class NoteDeleteView(DeleteView):
     model = Note
-    template_name = 'notes/delete_note.html'
-    success_url = reverse_lazy('notes')
-    context_object_name = 'notes'
+    template_name = 'notes/note_delete.html'
+    success_url = reverse_lazy('notes:notes')
+    context_object_name = 'note'
