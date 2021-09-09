71174123a45c346663633d10ceb144dbb2773016
Ilya Nesterov <eildos95@gmail.com>
2021-09-09 22:11:26 +0300

    set static routes

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+# Static settings
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
