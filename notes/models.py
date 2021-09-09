19d9eec76f7d4274515f190b02c8e3e9e4793f5e
Ilya Nesterov <eildos95@gmail.com>
2021-09-09 20:29:50 +0300

    create `Note` model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from PIL import Image
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    text = RichTextField(blank=True, null=True)
+    summary = models.CharField(max_length=100, default='', blank=True,
+                               help_text='Describe article in nutshell')
+    image = models.ImageField(blank=True, upload_to='picture_storage')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=255, unique=True,
+                            db_index=True, verbose_name="Url_slug")
+    source = models.URLField(blank=True, default='',
+                             help_text='If you want, put here your source link')
+    anonymous = models.BooleanField(
+        default=False, help_text='Others won\'t see that the note is yours.')
+
+    def __str__(self):
+        return self.title
