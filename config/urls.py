f32f3b1292aeab9464a7e26fcfc08fc16f2d9379
Ilya Nesterov <eildos95@gmail.com>
2021-09-10 18:34:22 +0300

    add default img, polsh templates

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('notes.urls')),
     path('accounts/', include('users.urls')),    
+    path('', include('notes.urls')),
 ]
