6f3eb925e7e345e97a9aff56c1f7123e78e5bfbf
Ilya Nesterov <eildos95@gmail.com>
2021-09-11 19:41:06 +0300

    set staticfiles_dirs, create footer, css

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,11 @@ USE_L10N = True
 USE_TZ = True
 # Static settings
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
