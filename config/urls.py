b9e05822dcff7794cf567d27e6c28f586b1fe493
sakkadas <eildos95@gmail.com>
2021-09-19 22:57:42 +0300

    implement custom error handlers

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views import defaults as default_views
+
+handler400 = 'config.views.bad_request'
+handler403 = 'config.views.permission_denied_view'
+handler404 = 'config.views.page_not_found'
+handler500 = 'config.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +17,29 @@ urlpatterns = [
     path('oauth/', include('social_django.urls', namespace='social')),
     path('notes_api/', include('notes_api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+
+    # allow display errors templates while DEBUG mode = True
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
