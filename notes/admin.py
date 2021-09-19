6493e85d57117607891c9144f9a16e54407e06b5
sakkadas <eildos95@gmail.com>
2021-09-19 20:57:02 +0300

    add fields to `NoteAdmin` panel

--- notes/admin.py
+++ notes/admin.py
@@ -5,8 +5,10 @@ from mptt.admin import MPTTModelAdmin
 
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created']
-
+    list_display = ['author', 'title', 'summary', 'created', 'source', 'total_likes', ]
+    list_filter = ('created', 'anonymous', )
+    search_fields = ('author',)
+    ordering = ('created',)
 
 admin.site.register(Note, NoteAdmin)
 
