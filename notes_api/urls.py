fa647f3eb7b1a6ad97f5ff4444fddf6a13e88d9e
sakkadas <eildos95@gmail.com>
2021-09-20 20:30:32 +0300

    create `CommentViewSet`, router

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
 from .views import *
 
 
 router = DefaultRouter()
 router.register('notes', NotesViewSet)
+router.register('comments', CommentViewSet)
 
 urlpatterns = router.urls
--- notes_api/views.py
+++ notes_api/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from notes.models import Note
-from .serializers import NoteSerializer
-from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
+from rest_framework.permissions import IsAuthenticated
 
+from notes.models import Note, Comment
+from .serializers import NoteSerializer, CommentSerializer
+from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 
 
 class NotesViewSet(viewsets.ModelViewSet):
     parser_classes = (MultiPartParser, FormParser, JSONParser,)
     queryset = Note.feature.all()
     serializer_class = NoteSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    parser_classes = (MultiPartParser, FormParser, JSONParser,)
+    permission_classes = IsAuthenticated
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
